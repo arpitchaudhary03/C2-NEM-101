f039e059123b581f6276ebc0e193a843ec4ac262
arpitchaudhary03 <coolarpit021@gmail.com>
2022-07-04 21:46:15 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = mongoose.connect("mongodb+srv://arpit:arpit123@cluster0.ep9ixn0.mongodb.net/fullstack?retryWrites=true&w=majority");
+const connection = mongoose.connect("mongodb+srv://localhost@cluster0.ep9ixn0.mongodb.net/fullstack?retryWrites=true&w=majority");
 require('dotenv').config();
 
 module.exports = connection;
